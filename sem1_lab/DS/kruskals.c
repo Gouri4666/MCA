577bc125b5e4507db00cebf8d2397154a1165f37
Gouri4666 <devagouri0803@gmail.com>
2025-10-26 11:37:26 +0530

    kruskals algorithm clean code

--- sem1_lab/DS/kruskals.c
+++ sem1_lab/DS/kruskals.c
@@ -1,104 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define MAX 100
 
 int parent[MAX];
 
-// Function to find the parent (with path compression)
+// Function to find parent (with path compression)
 int find(int i) {
     while (parent[i] != i)
         i = parent[i];
     return i;
 }
 
-// Function to perform union of two sets
+// Function to unite two sets
 void unionSets(int i, int j) {
-    int a = find(i);
-    int b = find(j);
-    parent[a] = b;
+    parent[i] = j;
 }
 
-int main() 
-{
-    int u[MAX], v[MAX], cost[MAX];
-    int n, e; // n = number of vertices, e = number of edges
-    int costMatrix[MAX][MAX] = {0};
-    int i, j, k;
+int main() {
+    int u[MAX], v[MAX], w[MAX];
+    int n, e, i, j;
     int mincost = 0;
 
-    printf("Enter the number of vertices: ");
+    printf("Enter number of vertices: ");
     scanf("%d", &n);
-    printf("Enter the number of edges: ");
+    printf("Enter number of edges: ");
     scanf("%d", &e);
 
     printf("Enter edges (u v cost):\n");
-    for (i = 0; i < e; i++) 
-    {
-        scanf("%d%d%d", &u[i], &v[i], &cost[i]);
-        // Build adjacency matrix
-        costMatrix[u[i]][v[i]] = cost[i];
-        costMatrix[v[i]][u[i]] = cost[i];
-    }
+    for (i = 0; i < e; i++)
+        scanf("%d%d%d", &u[i], &v[i], &w[i]);
 
-    // Sort edges by cost (Simple bubble sort)
-    for (i = 0; i < e - 1; i++) 
-    {
-        for (j = 0; j < e - i - 1; j++) 
-        {
-            if (cost[j] > cost[j + 1])
-            {
-                // Swap cost
-                int temp = cost[j];
-                cost[j] = cost[j + 1];
-                cost[j + 1] = temp;
-
-                // Swap u
-                temp = u[j];
-                u[j] = u[j + 1];
-                u[j + 1] = temp;
-
-                // Swap v
-                temp = v[j];
-                v[j] = v[j + 1];
-                v[j + 1] = temp;
+    // Sort edges by cost (simple bubble sort)
+    for (i = 0; i < e - 1; i++) {
+        for (j = 0; j < e - i - 1; j++) {
+            if (w[j] > w[j + 1]) {
+                int temp = w[j]; w[j] = w[j + 1]; w[j + 1] = temp;
+                temp = u[j]; u[j] = u[j + 1]; u[j + 1] = temp;
+                temp = v[j]; v[j] = v[j + 1]; v[j + 1] = temp;
             }
         }
     }
 
-    // Initialize each vertex as its own parent
+    // Initialize parent array
     for (i = 1; i <= n; i++)
         parent[i] = i;
 
-    printf("\nAdjacency Matrix:\n");
-    for (i = 1; i <= n; i++) 
-    {
-        for (j = 1; j <= n; j++) 
-        {
-            printf("%d\t", costMatrix[i][j]);
-        }
-        printf("\n");
-    }
+    printf("\nEdges in Minimum Spanning Tree:\n");
 
-    printf("\nEdges in the Minimum Cost Spanning Tree:\n");
-
-    int edgeCount = 0;
+    int count = 0;
     i = 0;
-    while (edgeCount < n - 1 && i < e) {
-        int setU = find(u[i]);
-        int setV = find(v[i]);
+    while (count < n - 1 && i < e) {
+        int a = find(u[i]);
+        int b = find(v[i]);
 
-        if (setU != setV) 
-        {
-            printf("Edge %d: (%d -> %d) cost = %d\n", edgeCount + 1, u[i], v[i], cost[i]);
-            mincost += cost[i];
-            edgeCount++;
-            unionSets(setU, setV);
+        if (a != b) {
+            printf("%d - %d : %d\n", u[i], v[i], w[i]);
+            mincost += w[i];
+            count++;
+            unionSets(a, b);
         }
         i++;
     }
 
-    printf("\nMinimum cost of spanning tree = %d\n", mincost);
-
+    printf("\nTotal Minimum Cost = %d\n", mincost);
     return 0;
 }
