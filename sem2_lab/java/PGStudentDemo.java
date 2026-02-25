7968915bb12780eb83ee433e24256e6722eaac5c
Gouri Nandana G <devagouri0803@gmail.com>
2026-02-25 05:43:20 +0530

    Update PGStudentDemo.java

--- sem2_lab/java/PGStudentDemo.java
+++ sem2_lab/java/PGStudentDemo.java
@@ -1,90 +1,130 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
-interface Showable 
+interface Showable
 {
-    void show();
+    public void show();
 }
-class Person 
+class Person
 {
     String name;
     String gender;
-    String phoneNo;
-    Person(String name, String gender, String phoneNo) 
+    long phone;
+    Person(String name,String gender,long phone)
     {
-        this.name = name;
-        this.gender = gender;
-        this.phoneNo = phoneNo;
+        this.name=name;
+        this.gender=gender;
+        this.phone=phone;
     }
 }
-
-class Student extends Person {
-
+class Student extends Person
+{
     String course;
-    double score;
-
-    Student(String name, String gender, String phoneNo,String course, double score)
+    int score;
+    Student(String name,String gender,long phone,String course,int score)
     {
-        super(name, gender, phoneNo);
-        this.course = course;
-        this.score = score;
+        super(name,gender,phone);
+        this.course=course;
+        this.score=score;
     }
 }
-
-class PGStudent extends Student implements Showable 
+class PGStudent extends Student implements Showable
 {
     String researchArea;
     String guide;
-    PGStudent(String name, String gender, String phoneNo,
-              String course, double score,
-              String researchArea, String guide) 
+    PGStudent(String name,String gender,long phone,String course,int score,String researchArea,String guide)
     {
-        super(name, gender, phoneNo, course, score);
-        this.researchArea = researchArea;
-        this.guide = guide;
+        super(name,gender,phone,course,score);
+        this.researchArea=researchArea;
+        this.guide=guide;
     }
-
-    public void show() 
+    public void show()
     {
-        System.out.println("Name          : " + name);
-        System.out.println("Course        : " + course);
-        System.out.println("Score         : " + score);
-        System.out.println("Research Area : " + researchArea);
-        System.out.println("Guide         : " + guide);
-        System.out.println("--------------------------------");
+        System.out.println("Name: "+name);
+        System.out.println("Gender: "+gender);
+        System.out.println("Phone: "+phone);
+        System.out.println("Course: "+course);
+        System.out.println("Score: "+score);
+        System.out.println("Research Area: "+researchArea);
+        System.out.println("Guide: "+guide);
+                
     }
 }
-public class PGStudentDemo 
+
+public class PGStudentDemo
 {
-    public static void main(String[] args)
-     {
-
-        PGStudent[] pg = 
+    public static void main(String [] args)
+    {
+        PGStudent [] p=new PGStudent[3];
+        p[0]=new PGStudent("Gouri","female",9745245,"MCA",45,"CS","Kiran");
+        p[1]=new PGStudent("Prita","female",9745245,"MCA",78,"Mal","Kiran");
+        p[2]=new PGStudent("Gouri","female",9745245,"MCA",23,"ACN","Kiran");
+        //rank on score
+        System.out.println("rank on score");
+        Arrays.sort(p,(s1,s2)->Integer.compare(s2.score,s1.score));
+        for(int i=0;i<p.length;i++)
         {
-            new PGStudent("Anu", "Female", "9876543210",
-                    "MCA", 85, "AI", "Dr. Kumar"),
-
-            new PGStudent("Rahul", "Male", "9123456780",
-                    "MCA", 92, "Data Science", "Dr. Smith"),
-
-            new PGStudent("Meera", "Female", "9988776655",
-                    "MCA", 78, "Cyber Security", "Dr. John")
-        };
-
-        Arrays.sort(pg, (s1, s2) -> Double.compare(s2.score, s1.score));
-
-        System.out.println("=== Ranking Based on Score ===");
-        for (int i = 0; i < pg.length; i++) 
-        {
-            System.out.println("Rank " + (i + 1));
-            pg[i].show();
+            System.out.println("\nRank"+(i+1));
+            p[i].show();
         }
-
-        Arrays.sort(pg, Comparator.comparing(s -> s.researchArea));
-        System.out.println("=== Sorted Based on Research Area ===");
-        for (PGStudent s : pg) 
+        //sort on reaseach area
+        System.out.println("\nSort on research area");
+        Arrays.sort(p,Comparator.comparing(s->s.researchArea));
+        for(int i=0;i<p.length;i++)
         {
-            s.show();
+            p[i].show();
         }
     }
 }
+
+
+// rank on score
+
+// Rank1
+// Name: Prita
+// Gender: female
+// Phone: 9745245
+// Course: MCA
+// Score: 78
+// Research Area: Mal
+// Guide: Kiran
+
+// Rank2
+// Name: Gouri
+// Gender: female
+// Phone: 9745245
+// Course: MCA
+// Score: 45
+// Research Area: CS
+// Guide: Kiran
+
+// Rank3
+// Name: Gouri
+// Gender: female
+// Phone: 9745245
+// Course: MCA
+// Score: 23
+// Research Area: ACN
+// Guide: Kiran
+
+// Sort on research area
+// Name: Gouri
+// Gender: female
+// Phone: 9745245
+// Course: MCA
+// Score: 23
+// Research Area: ACN
+// Guide: Kiran
+// Name: Gouri
+// Gender: female
+// Phone: 9745245
+// Course: MCA
+// Score: 45
+// Research Area: CS
+// Guide: Kiran
+// Name: Prita
+// Gender: female
+// Phone: 9745245
+// Course: MCA
+// Score: 78
+// Research Area: Mal
+// Guide: Kiran
