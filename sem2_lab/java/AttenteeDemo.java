215ec223c6f47f37221963a8cac8c61b1e88b144
Gouri Nandana G <devagouri0803@gmail.com>
2026-03-10 19:51:15 +0530

    Update hashmap

--- sem2_lab/java/AttenteeDemo.java
+++ sem2_lab/java/AttenteeDemo.java
@@ -1,76 +1,46 @@
 import java.util.*;
-
 class Attendee {
-
     String name;
     String email;
-    int registrationId;
-
-    Attendee(String name, String email, int registrationId) {
+    Attendee(String name, String email) {
         this.name = name;
         this.email = email;
-        this.registrationId = registrationId;
-    }
-
-    public int hashCode() {
-        return registrationId;
-    }
-
-    public boolean equals(Object obj) {
-        Attendee a = (Attendee) obj;
-        return this.registrationId == a.registrationId;
     }
 }
-
-public class AttenteeDemo {
-
+public class AttendeeDemo {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        HashSet<Attendee> attendees = new HashSet<>();
-
+        HashMap<Integer, Attendee> attendees = new HashMap<>();
         int choice;
-
         do {
-
             System.out.println("\n1 Add Attendee");
             System.out.println("2 Display Attendees");
             System.out.println("3 Exit");
-
             System.out.print("Enter choice: ");
             choice = sc.nextInt();
             sc.nextLine();
-
             switch(choice) {
-
                 case 1:
-
-                    System.out.print("Enter Name: ");
-                    String name = sc.nextLine();
-
-                    System.out.print("Enter Email: ");
-                    String email = sc.nextLine();
-
                     System.out.print("Enter Registration ID: ");
                     int id = sc.nextInt();
                     sc.nextLine();
-
-                    Attendee a = new Attendee(name, email, id);
-
-                    if(attendees.add(a))
-                        System.out.println("Registered Successfully");
-                    else
+                    if(attendees.containsKey(id)) {
                         System.out.println("Duplicate Registration ID");
-
+                        break;
+                    }
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter Email: ");
+                    String email = sc.nextLine();
+                    Attendee a = new Attendee(name, email);
+                    attendees.put(id, a);
+                    System.out.println("Registered Successfully");
                     break;
-
                 case 2:
-
                     System.out.println("\nAttendee List:");
 
-                    for(Attendee at : attendees)
-                        System.out.println(at.registrationId + " " + at.name + " " + at.email);
-
+                        for(Integer id : attendees.keySet())
+                    System.out.println(id + " " + attendees.get(id).name + " " + attendees.get(id).email);
                     break;
 
                 case 3:
@@ -79,7 +49,6 @@ public class AttenteeDemo {
 
                 default:
                     System.out.println("Invalid choice");
-
             }
 
         } while(choice != 3);
