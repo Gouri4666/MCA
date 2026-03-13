931418adde62a082121aaf3380f9ac94d7f71814
Gouri Nandana G <devagouri0803@gmail.com>
2026-03-13 10:57:19 +0530

    multithread

--- sem2_lab/java/ThreadDemo.java
+++ sem2_lab/java/ThreadDemo.java
@@ -58,3 +58,28 @@ public class ThreadDemo
         System.out.println("Calculation completed!");
     }    
 }
+
+//output
+// ThreadA started execution
+// ThreadB started execution
+// ThreadA: Square of 1 = 1
+// Thread B: Cube of 1 = 1
+// ThreadA: Square of 2 = 4
+// Thread B: Cube of 2 = 8
+// ThreadA: Square of 3 = 9
+// Thread B: Cube of 3 = 27
+// ThreadA: Square of 4 = 16
+// ThreadA: Square of 5 = 25
+// Thread B: Cube of 4 = 64
+// ThreadA: Square of 6 = 36
+// Thread B: Cube of 5 = 125
+// ThreadA: Square of 7 = 49
+// Thread B: Cube of 6 = 216
+// ThreadA: Square of 8 = 64
+// ThreadA: Square of 9 = 81
+// Thread B: Cube of 7 = 343
+// ThreadA: Square of 10 = 100
+// Thread B: Cube of 8 = 512
+// Thread B: Cube of 9 = 729
+// Thread B: Cube of 10 = 1000
+// Calculation completed
