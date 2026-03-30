d6700261dc69da7853680c282b62994ac163830c
Gouri Nandana G <devagouri0803@gmail.com>
2026-03-30 13:41:14 +0530

    Update ProcessThread.c

--- sem2_lab/os/ProcessThread.c
+++ sem2_lab/os/ProcessThread.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-
-// Thread function
-void* thread_function(void* arg) {
-    printf("Thread running in process with PID: %d\n", getpid());
-    return NULL;
+void* fun1(void* arg) {
+    printf("Thread 1 is running in PID = %d\n", getpid());
+    return 0;
 }
-
+void* fun2(void* arg) {
+    printf("Thread 2 is running in PID = %d\n", getpid());
+    return 0;
+}
 int main() {
-    pid_t pid;
-    pthread_t t1;
-
-    pid = fork();  // Create process
-
-    if (pid < 0) {
-        printf("Fork failed!\n");
-        return 1;
-    }
-
-    // Both parent and child execute this
+    pthread_t t1, t2;
+    int pid;
+    pid = fork();  // Create a new process
     if (pid == 0) {
-        // Child process
-        printf("\n--- Child Process ---\n");
-        printf("Child PID: %d\n", getpid());
-
-        pthread_create(&t1, NULL, thread_function, NULL);
+        printf("Child process created. PID = %d\n", getpid());
+        pthread_create(&t1, NULL, fun1, NULL);
+        pthread_create(&t2, NULL, fun2, NULL);
         pthread_join(t1, NULL);
-    }
+        pthread_join(t2, NULL);
+        printf("Child process ends\n");
+    } 
     else {
-        // Parent process
-        printf("\n--- Parent Process ---\n");
-        printf("Parent PID: %d\n", getpid());
-        printf("Child PID: %d\n", pid);
-
-        pthread_create(&t1, NULL, thread_function, NULL);
+        printf("Parent process created. PID = %d\n", getpid());
+        pthread_create(&t1, NULL, fun1, NULL);
+        pthread_create(&t2, NULL, fun2, NULL);
         pthread_join(t1, NULL);
+        pthread_join(t2, NULL);
+        printf("Parent process ends\n");
     }
 
     return 0;
