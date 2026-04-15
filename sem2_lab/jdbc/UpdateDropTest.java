7a357ee629f4d4d1c0ca8f77ffb615ab24a24d74
Gouri Nandana G <devagouri0803@gmail.com>
2026-04-15 12:37:34 +0530

    All query

--- sem2_lab/jdbc/UpdateDropTest.java
+++ sem2_lab/jdbc/UpdateDropTest.java
@@ -1,59 +1,170 @@
 import java.sql.*;
-
-public class UpdateDropTest {
+import java.util.*;
+class AllinOne
+{
     public static void main(String[] args) {
-        try {
-            // 1. Load driver
+        Scanner sc=new Scanner(System.in);
+        try
+        {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-
-            // 2. Create connection
-            Connection con = DriverManager.getConnection(
-                "jdbc:oracle:thin:@localhost:1521/XEPDB1",
-                "gouri",
-                "gouri123"
+            Connection con=DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe",
+                "system",
+                "root"
             );
-
-            System.out.println("Connected Successfully!");
-
-            // 3. UPDATE using PreparedStatement 🔥
-            String updateQuery = "UPDATE student SET name = ?, age = ? WHERE id = ?";
-
-            PreparedStatement ps = con.prepareStatement(updateQuery);
-
-            ps.setString(1, "Alex");
-            ps.setInt(2, 25);
-            ps.setInt(3, 1);   // update record with id = 1
-
-            int rows = ps.executeUpdate();
-
-            if (rows > 0) {
-                System.out.println("Record Updated Successfully!");
-            } else {
-                System.out.println("No record found to update!");
+            System.out.println("Connected!");
+            //create table
+            Statement s=con.createStatement();
+            try
+            {
+                s.executeUpdate("create table book(id number,title varchar2(25),author varchar2(25),price number)");
+                System.out.println("table created!");
             }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            //insert value
+            try
+            {
+                PreparedStatement ps=con.prepareStatement("insert into book(id,title,author,price) values (?,?,?,?)");
+                ps.setInt(1,1);
+                ps.setString(2,"WOF");
+                ps.setString(3, "APJ");
+                ps.setInt(4,650);
+                ps.executeUpdate();
+                System.out.println("value inserted");
 
-            // 4. DROP TABLE
-            Statement stmt = con.createStatement();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            //display
+            try
+            {
+                ResultSet rs=s.executeQuery("select * from book");
+                boolean found=false;
+                while(rs.next())
+                {
+                found=true;
+                int id=rs.getInt("id");
+                String title=rs.getString("title");
+                String author=rs.getString("author");
+                int price=rs.getInt("price");
+                System.out.println(id+" "+title+" "+author+" "+price);
+                }   
+                if(!found)
+                {
+                    System.out.println("Not found!");
+                }
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            //update price
+            try
+            {
+                PreparedStatement ps2=con.prepareStatement("update book set price=? where id=?");
+                ps2.setInt(1,700);
+                ps2.setInt(2,10);
+                int update=ps2.executeUpdate();
+                if(update>0)
+                {
+                    System.out.println("Price updated");
+                }else
+                {
+                    System.out.println("Updateion failed");
+                }
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            //insert one value too
+            try
+            {
+                PreparedStatement ps3=con.prepareStatement("insert into book(id,title,author,price) values (?,?,?,?)");
+                ps3.setInt(1,2);
+                ps3.setString(2,"Freedom");
+                ps3.setString(3,"MGandhi");
+                ps3.setInt(4,540);
+                ps3.executeUpdate();
+                System.out.println("one row inserted");
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            //display
+            try
+            {
+                ResultSet rs2=s.executeQuery("select * from book");
+                boolean found=false;
+                while(rs2.next())
+                {
+                    found=true;
+                    int id=rs2.getInt("id");
+                    String title=rs2.getString("title");
+                    String author=rs2.getString("author");
+                    int price=rs2.getInt("price");                    
+                    System.out.println(id+" "+title+" "+author+" "+price);
+                }
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            //diaply book with id 2
+            try
+            {
+                PreparedStatement ps4=con.prepareStatement("select * from book where id=?");
+                ps4.setInt(1,2);
+                ResultSet rs3=ps4.executeQuery();
+                
+                boolean found=false;
+                while(rs3.next())
+                {
+                    found=true;
+                    int id=rs3.getInt("id");
+                    String title=rs3.getString("title");
+                    String author=rs3.getString("author");
+                    int price=rs3.getInt("price");
+                    System.out.println(id+" "+title+" "+author+" "+price);
+                }
+                if(!found)
+                {
+                    System.out.println("not found!");
+                }
 
-            String dropQuery = "DROP TABLE student";
-
-            stmt.executeUpdate(dropQuery);
-            System.out.println("Table Dropped Successfully!");
-
-            // 5. Close connection
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            //delete book with id=1
+            try
+            {
+                PreparedStatement ps5=con.prepareStatement("delete from book where id=?");
+                ps5.setInt(1,1);
+                ps5.executeUpdate();
+                System.out.println("Deleted row with id=1");
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            //drop table
+            PreparedStatement ps6=con.prepareStatement("drop table book");
+            ps6.executeUpdate();
+            System.out.println("table droped!");
             con.close();
-
-        } catch (Exception e) {
-            System.out.println(e);
+            sc.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
         }
     }
 }
-
-/*
-output
-
-Connected Successfully!
-Record Updated Successfully!
-Table Dropped Successfully!
-
-*/
