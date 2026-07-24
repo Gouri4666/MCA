6d4f5ed6d4f0159998ff98cae5e27433f3518b9c
Gouri Nandana G <devagouri0803@gmail.com>
2026-07-24 11:50:18 +0530

    Create SharedApp2.java

---
+++ sem3_lab/Andoid_studio/3.SharedPreferences/SharedApp2.java
@@ -0,0 +1,45 @@
+package com.example.sharedapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public abstract class SharedApp2 extends AppCompatActivity implements View.OnClickListener {
+    Button prev;
+    TextView t;
+    SharedPreferences sp;
+
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_shared_app2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        prev=(Button) findViewById((R.id.prev));
+        sp=getSharedPreferences("MyData",MODE_PRIVATE);
+        t=(TextView) findViewById(R.id.namestring);
+        if (sp.contains("Name"))
+            t.setText("WELCOME "+sp.getString("Name",""));
+
+    }
+    public void onClick(View v)
+    {
+        Intent i=new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+}
