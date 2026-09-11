2e60ed071f71ff0382b6236adaadb27c516d6693
Gouri Nandana G <devagouri0803@gmail.com>
2026-09-11 07:39:27 +0530

    Create MainActivity.java

---
+++ sem3_lab/Andoid_studio/9. Grid Layout/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.grid_calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    TextView display;
+    double firstNumber = 0;
+    double secondNumber = 0;
+    String operator = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        display = findViewById(R.id.display);
+    }
+    public void onClick(View view) {
+
+        int id = view.getId();
+
+        if (id == R.id.clear) {
+            display.setText("0");
+            firstNumber = 0;
+            secondNumber = 0;
+            operator = "";
+        }
+        else if (id == R.id.zero) {
+            addNumber("0");
+        }
+        else if (id == R.id.one) {
+            addNumber("1");
+        }
+        else if (id == R.id.two) {
+            addNumber("2");
+        }
+        else if (id == R.id.three) {
+            addNumber("3");
+        }
+        else if (id == R.id.four) {
+            addNumber("4");
+        }
+        else if (id == R.id.five) {
+            addNumber("5");
+        }
+        else if (id == R.id.six) {
+            addNumber("6");
+        }
+
+        else if (id == R.id.seven) {
+            addNumber("7");
+        }
+
+        else if (id == R.id.eight) {
+            addNumber("8");
+        }
+
+        else if (id == R.id.nine) {
+            addNumber("9");
+        }
+       else if (id == R.id.dot) {
+            String value = display.getText().toString();
+            if (!value.contains(".")) {
+                if (value.equals("0")) {
+                    display.setText("0.");
+                }
+                else {
+                    display.append(".");
+                }
+            }
+        }
+        else if (id == R.id.add) {
+            firstNumber =
+                    Double.parseDouble(getCurrentNumber());
+            operator = "+";
+            display.setText(
+                    getCurrentNumber() + " + "
+            );
+        }
+        else if (id == R.id.diff) {
+            firstNumber =
+                    Double.parseDouble(getCurrentNumber());
+            operator = "-";
+            display.setText(
+                    getCurrentNumber() + " - "
+            );
+        }
+        else if (id == R.id.mul) {
+            firstNumber =
+                    Double.parseDouble(getCurrentNumber());
+            operator = "*";
+            display.setText(
+                    getCurrentNumber() + " * "
+            );
+        }
+        else if (id == R.id.div) {
+            firstNumber =
+                    Double.parseDouble(getCurrentNumber());
+            operator = "/";
+            display.setText(
+                    getCurrentNumber() + " / "
+            );
+        }
+        else if (id == R.id.equal) {
+            String expression = display.getText().toString();
+            if (operator.equals("")) {
+                return;
+            }
+            String[] parts = expression.split(" ");
+            if (parts.length < 3) {
+                return;
+            }
+            secondNumber =
+                    Double.parseDouble(parts[2]);
+            double result = 0;
+            if (operator.equals("+")) {
+                result = firstNumber + secondNumber;
+            }
+            else if (operator.equals("-")) {
+                result = firstNumber - secondNumber;
+            }
+            else if (operator.equals("*")) {
+                result = firstNumber * secondNumber;
+            }
+            else if (operator.equals("/")) {
+                if (secondNumber == 0) {
+                    display.setText("Error");
+                    operator = "";
+                    return;
+               }
+                result = firstNumber / secondNumber;
+            }
+            display.setText(String.valueOf(result));
+            operator = "";
+        }
+    }
+
+    // Add number to display
+
+    public void addNumber(String number) {
+
+        String current = display.getText().toString();
+
+        // If display is 0
+        if (current.equals("0")) {
+
+            display.setText(number);
+        }
+
+        // If display contains an operator
+        else if (current.endsWith("+ ")
+                || current.endsWith("- ")
+                || current.endsWith("* ")
+                || current.endsWith("/ ")) {
+
+            display.append(number);
+        }
+
+        // Normal number
+        else {
+
+            display.append(number);
+        }
+    }
+
+    // Get the current number
+
+    public String getCurrentNumber() {
+
+        String expression = display.getText().toString();
+
+        String[] parts = expression.split(" ");
+
+        if (parts.length == 1) {
+            return parts[0];
+        }
+
+        return parts[parts.length - 1];
+    }
+}
